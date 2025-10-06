abe33f4681a5c6328a38ab76c983ea811a5be398
Ialbertine <a.ingabire1@alustudent.com>
2025-10-06 20:14:53 +0200

    fix: configuration issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,29 @@
-import js from '@eslint/js';
+const js = require("@eslint/js");
 
-export default [
+module.exports = [
   js.configs.recommended,
   {
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'commonjs',
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
       globals: {
-        console: 'readonly',
-        process: 'readonly',
-        Buffer: 'readonly',
-        __dirname: 'readonly',
-        __filename: 'readonly',
-        global: 'readonly',
-        module: 'readonly',
-        require: 'readonly',
-        exports: 'readonly'
-      }
+        console: "readonly",
+        process: "readonly",
+        Buffer: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        global: "readonly",
+        module: "readonly",
+        require: "readonly",
+        exports: "readonly",
+      },
     },
-    files: ['src/**/*.js'],
+    files: ["src/**/*.js"],
     rules: {
       "no-console": "off",
-      'no-unused-vars': 'error',
-      'no-var': 'error',
-      'prefer-const': 'error'
-    }
-  }
+      "no-unused-vars": "error",
+      "no-var": "error",
+      "prefer-const": "error",
+    },
+  },
 ];
